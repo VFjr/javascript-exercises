6b839f23adaab90a96b8825bed4bfc2734acd949
Victor <victor@engineeredarts.co.uk>
2024-08-29 23:30:39 +0100

    Completed Exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (countArg) {
+    if (countArg < 0) {
+        return "OOPS";
+    }
+    else if (countArg == 0) {
+        return 0;
+    }
+    else if (countArg <= 2) {
+        return 1;
+    }
 
+    var a = 1;
+    var b = 1;
+
+    for (i = 3; i <= countArg; i++) {
+        next_term = a + b;
+        a = b;
+        b = next_term;
+        console.log(`${a}, ${b}`);
+    }
+
+    return b;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,37 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+
+    return people.reduce((last_oldest_person, current_person) => {
+
+        if (last_oldest_person === undefined) {
+            return current_person;
+        }
+
+        last_age = get_age(last_oldest_person);
+        current_age = get_age(current_person);
+
+        if (current_age > last_age) {
+            return current_person;
+        }
+        else {
+            return last_oldest_person;
+        }
+    }, undefined)
+
+
+
 
 };
+
+const get_age = function (person) {
+
+    var birth_date = person.yearOfBirth;
+    var death_date = person.yearOfDeath;
+    if (death_date === undefined) {
+        today = new Date();
+        death_date = today.getFullYear();
+    }
+    return death_date - birth_date;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
